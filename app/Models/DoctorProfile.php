a0e1d30e904567b3caed465621c781c701d383c0
simone-nolberto <simone.nolberto@outlook.com>
2024-06-14 10:20:11 +0200

    update with doctor_profile_specialization table
    add table for relationship between doctor_profile and specializations

--- app/Models/DoctorProfile.php
+++ app/Models/DoctorProfile.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class DoctorProfile extends Model
 {
     use HasFactory;
     protected $fillable = ['cv', 'photo', 'address', 'telephone', 'services'];
+
+    public function specializations(): BelongsToMany
+    {
+        return $this->belongsToMany(Specialization::class);
+    }
 }
--- app/Models/Specialization.php
+++ app/Models/Specialization.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Specialization extends Model
 {
     use HasFactory;
+
+    public function doctorProfiles(): BelongsToMany
+    {
+        return $this->belongsToMany(DoctorProfile::class);
+    }
 }
