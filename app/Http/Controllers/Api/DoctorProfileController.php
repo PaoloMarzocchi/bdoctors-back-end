5080f94a60ef7549fa44cf20025ba3c213dae6d5
marinodilauro <m.dilauro90@gmail.com>
2024-06-24 23:14:24 +0200

    Add advanced search

--- app/Http/Controllers/Api/DoctorProfileController.php
+++ app/Http/Controllers/Api/DoctorProfileController.php
@@ -60,9 +60,12 @@ class DoctorProfileController extends Controller
 
 
         //SELEZIONATI PER SPECIALIZZAZIONE ORDINATI PER SPONSORSHIP
-        $searchResults = DoctorProfile::with('specializations', 'sponsorships', 'user', 'votes')->whereRelation('specializations', 'name', '=', $name)->leftJoin('doctor_profile_sponsorship', 'doctor_profiles.id', '=', 'doctor_profile_sponsorship.doctor_profile_id')
+        $searchResults = DoctorProfile::with('specializations', 'sponsorships', 'user', 'votes')
+            ->whereRelation('specializations', 'name', '=', $name)
+            ->leftJoin('doctor_profile_sponsorship', 'doctor_profiles.id', '=', 'doctor_profile_sponsorship.doctor_profile_id')
             ->select('doctor_profiles.*', DB::raw('IF(doctor_profile_sponsorship.sponsorship_id IS NOT NULL, 1, 0) as has_sponsorship'))
-            ->orderBy('has_sponsorship', 'desc')->distinct()
+            ->orderBy('has_sponsorship', 'desc')
+            ->distinct()
             ->get();
 
         return response()->json(['success' => true, 'searchResults' => $searchResults]);
@@ -74,12 +77,35 @@ class DoctorProfileController extends Controller
 
         // dd($specialization, $vote, $review);
 
-        // SELEZIONATI PER SPECIALIZZAZIONE ORDINATI PER SPONSORSHIP
-        $searchResults = DoctorProfile::with('specializations', 'sponsorships', 'user', 'votes', 'reviews')->whereRelation('specializations', 'name', '=', $specialization)->whereRelation('votes', 'vote', '=', $minAverageVote)->leftJoin('doctor_profile_sponsorship', 'doctor_profiles.id', '=', 'doctor_profile_sponsorship.doctor_profile_id')
+        /*         $searchResults = DoctorProfile::with('specializations', 'sponsorships', 'user', 'votes', 'reviews')
+            ->whereRelation('specializations', 'name', '=', $specialization)
+            ->whereRelation('votes', 'vote', '=', $minAverageVote)
+            ->leftJoin('doctor_profile_sponsorship', 'doctor_profiles.id', '=', 'doctor_profile_sponsorship.doctor_profile_id')
             ->select('doctor_profiles.*', DB::raw('IF(doctor_profile_sponsorship.sponsorship_id IS NOT NULL, 1, 0) as has_sponsorship'))
-            ->orderBy('has_sponsorship', 'desc')->distinct()
-            ->get(); //questa è quella corretta!!
+            ->orderBy('has_sponsorship', 'desc')
+            ->distinct()
+            ->get(); */ //questa è quella corretta!!
 
+
+        /* Questa funziona come URL ma dà CORS policy error se si filtrano i risultati*/
+        $searchResults =
+            DoctorProfile::with('specializations', 'sponsorships', 'user', 'votes')
+            ->join('users', 'doctor_profiles.user_id', '=', 'users.id')
+            ->join('doctor_profile_specialization', 'doctor_profiles.id', '=', 'doctor_profile_specialization.doctor_profile_id')
+            ->join('specializations', 'doctor_profile_specialization.specialization_id', '=', 'specializations.id')
+            ->leftJoin('doctor_profile_vote', 'doctor_profiles.id', '=', 'doctor_profile_vote.doctor_profile_id')
+            ->leftJoin('votes', 'doctor_profile_vote.vote_id', '=', 'votes.id')
+            ->leftJoin('reviews', 'doctor_profiles.id', '=', 'reviews.doctor_profile_id')
+            ->select('doctor_profiles.*', DB::raw('AVG(votes.vote) as average_vote'), DB::raw('COUNT(reviews.id) as review_count'))
+            ->where('specializations.name', $specialization)
+            ->groupBy('doctor_profiles.id')
+            ->having('average_vote', '>=', $minAverageVote)
+            ->having('review_count', '>=', $minTotalReviews)
+            ->get();
+
+
+
+        // dd($searchResults);
 
         // $searchResults = DoctorProfile::with('specializations', 'sponsorships', 'user', 'votes', 'reviews')
         //     ->whereRelation('specializations', 'name', '=', $specialization)
