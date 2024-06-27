63e4fa94532ace4ad0cd59f56396cb3f7dd9155f
SimoneCerri <simone.cerri1994@gmail.com>
2024-06-27 12:24:56 +0200

    replace the config-title with a normal one, change icon and nameapp when logged

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,25 +1,27 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-  <head>
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'BDoctors') }}</title>
+    {{-- <title>{{ config('app.name', 'BDoctors') }}</title> --}}
+    <title>Bdoctors</title>
+    <link rel="icon" href="/img/BDoctors_transparent.png">
 
     <!-- Fontawesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
-      integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
-      crossorigin="anonymous" referrerpolicy="no-referrer" />
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
-      rel="stylesheet">
+        rel="stylesheet">
 
     <!-- Using BrainTree script -->
     <script src="https://js.braintreegateway.com/web/dropin/1.24.0/js/dropin.min.js"></script>
@@ -27,25 +29,25 @@
 
     <!-- Usando Vite -->
     @vite(['resources/js/app.js'])
-  </head>
+</head>
 
-  <body>
+<body>
     <div id="app">
 
-      <main>
-        <div class="dashboard_container d-flex">
+        <main>
+            <div class="dashboard_container d-flex">
 
-          <!-- Sidebar -->
-          @include('partials.sidebar', ['doctorProfile' => $doctorProfile])
+                <!-- Sidebar -->
+                @include('partials.sidebar', ['doctorProfile' => $doctorProfile])
 
-          <div class="view position-relative flex-fill">
+                <div class="view position-relative flex-fill">
 
-            @yield('content')
+                    @yield('content')
 
-          </div>
-        </div>
-      </main>
+                </div>
+            </div>
+        </main>
     </div>
-  </body>
+</body>
 
 </html>
