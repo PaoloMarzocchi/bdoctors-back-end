b0003b230f16567a7b8d753e5b9704fae983fae6
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-06-30 14:14:29 +0200

    edit layout payment.blade, add loading, delete submit button after loading

--- resources/views/admin/sponsorship/payment.blade.php
+++ resources/views/admin/sponsorship/payment.blade.php
@@ -1,37 +1,36 @@
 @extends('layouts.admin')
 
 @section('content')
+    {{ $loading = false }}
+    {{-- Bottone per sidebar --}}
+    <button class="btn rounded border mb-3 m-1 d-lg-none" type="button" data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+        <i class="fa-solid fa-bars"></i>
+    </button>
+    <div class="container pt-5">
+        <div id="loading"></div>
 
-    <div class="container py-5">
-
-        {{-- Bottone per sidebar --}}
-        <button class="btn rounded border mb-3 d-lg-none" type="button" data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
-            <i class="fa-solid fa-bars"></i>
-        </button>
-
-        <div class="d-flex justify-content-center">
-            <div class="card w-25 bg-body-secondary">
+        <div id="payment-container" class="d-flex justify-content-center">
+            <div class="card bg-body-secondary">
                 @csrf
-                <div class="card-body">
+                <div id="card-payment" class="card-body">
                     <div id="dropin-wrapper">
                         <div id="checkout-message">
-                            You select :<span class="my_primary fw-bold"> {{ $sponsorship->name }}</span>
+                            You have selected : <br> <span class="my_primary fw-bold"> {{ $sponsorship->name }}</span>
                         </div>
                         <div id="dropin-container">
 
                         </div>
-                        <div class="d-flex justify-content-between align-items-center my-2">
-                            <button id="submit-button" type="submit"
-                                class="btn my_btn_primary btn-lg px-4 rounded-pill my-3">
-                                Submit
-                                payment
-                                <i class="ms-2 fa-solid fa-arrow-right"></i>
-                            </button>
+                        <div class="d-flex flex-column align-items-center my-2">
                             <div class="card-title">
                                 Checkout:
                                 <span class="text-success fw-bold">{{ $sponsorship->price }} €</span>
                             </div>
+                            <button id="submit-button" type="submit"
+                                class="btn my_btn_primary btn-md px-4 rounded-pill my-3">
+                                Submit
+                            </button>
+
                         </div>
                     </div>
                 </div>
@@ -75,45 +74,82 @@
                 },
                 function(err, instance) {
                     button.addEventListener('click', function() {
+
                         instance.requestPaymentMethod(function(err, payload) {
-                            (function($) {
-                                $(function() {
-                                    $.ajaxSetup({
-                                        headers: {
-                                            'X-CSRF-TOKEN': $(
-                                                    'meta[name="csrf-token"]')
-                                                .attr('content')
-                                        }
-                                    });
-                                    $.ajax({
-                                        type: "POST",
-                                        url: "{{ route('admin.token', $sponsorship) }}",
-                                        data: {
-                                            nonce: payload.nonce
-                                        },
-                                        success: function(data) {
-                                            console.log('success', payload
-                                                .nonce)
-                                            $('#checkout-message').html(
-                                                `<h1 class="text-success">Success !</h1>
 
-                                            <p>Congratulation, you add a sponsorship to your profile !</p>
-                                            <p>Add more time to your existing sponsorship : </p>
-                                            <a class="btn my_btn_primary btn-lg px-4 rounded-pill my-3" href="{{ route('admin.sponsorship.index') }}">
-                                                Sponsorships
-                                            </a>`
+                            if (typeof payload !== 'undefined') {
 
-                                            );
-                                        },
-                                        error: function(data) {
-                                            console.log('error', payload.nonce)
-                                            $('#checkout-message').html(
-                                                '<h1>Error</h1><p>Check your payment method and try again</p>'
+                                $('#submit-button').remove();
+
+                                (function($) {
+                                    $(function() {
+
+                                        let loading = "{{ $loading }}";
+                                        loading = true;
+
+                                        if (loading) {
+
+                                            $('#loading').html(
+                                                `<div class="d-flex justify-content-center my-5">
+                                                <div class="spinner-border" role="status">
+                                                    <span class="visually-hidden">Caricamento...</span>
+                                                </div>
+                                                </div>`
                                             );
                                         }
+
+                                        $.ajaxSetup({
+
+                                            headers: {
+                                                'X-CSRF-TOKEN': $(
+                                                        'meta[name="csrf-token"]')
+                                                    .attr('content')
+                                            }
+                                        });
+
+                                        $.ajax(
+
+                                            {
+                                                type: "POST",
+                                                url: "{{ route('admin.token', $sponsorship) }}",
+                                                data: {
+                                                    nonce: payload.nonce
+                                                },
+                                                success: function(data) {
+                                                    console.log('success', payload
+                                                        .nonce)
+                                                    $('#checkout-message').html(
+                                                        `<h1 class="text-success">Success !</h1>
+                                                    <p>Congratulation, you add <span class="my_primary fw-bold"> {{ $sponsorship->name }}</span> to your profile !</p>
+                                                    <p>Add more time to your existing sponsorship : </p>
+                                                    <a class="btn my_btn_primary btn-md px-4 rounded-pill my-3" href="{{ route('admin.sponsorship.index') }}">
+                                                        Sponsorships
+                                                    </a>`
+
+                                                    );
+
+                                                    loading = false;
+                                                    $('#loading').remove();
+
+
+                                                },
+                                                error: function(data) {
+                                                    console.log('error', payload
+                                                        .nonce)
+                                                    $('#checkout-message').html(
+                                                        '<h1>Error</h1><p>Check your payment method and try again</p>'
+                                                    );
+                                                    loading = false;
+                                                    $('#loading').remove();
+                                                }
+                                            }
+
+                                        );
+
                                     });
-                                });
-                            })(jQuery);
+
+                                })(jQuery);
+                            }
                         });
                     });
                 });
